714cdfaf753233d28be72a85f73dc11a7c8e23ba
DAVIDE <davideliuzzo189@gmail.com>
2024-11-02 16:46:03 +0100

    Creazione carica e salva da cache remota

---
+++ carica_salva.js
@@ -0,0 +1,42 @@
+//import myToken, myKey from  .....
+
+
+export function carica() {
+    return fetch('https://ws.progettimolinari.it/cache/get', {
+            headers: {
+                'Content-Type': 'application/json',
+                key: myToken,
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                key: myKey,
+            }),
+        })
+        .then((r) => r.json())
+        .then((r) => {
+            console.log('Dati caricati:', r.result);
+            diz = r.result || {};
+            render();
+        })
+        .catch((err) => console.log('Errore durante il caricamento:', err));
+}
+
+export function salva() {
+    return fetch('https://ws.progettimolinari.it/cache/set', {
+            headers: {
+                'Content-Type': 'application/json',
+                key: myToken,
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                key: myKey,
+                value: diz,
+            }),
+        })
+        .then((r) => r.json())
+        .then((r) => {
+            console.log('Dati salvati:', r);
+            return r;
+        })
+        .catch((err) => console.log('Errore durante il salvataggio:', err));
+}
